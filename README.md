6ae82afed14f2b71191eeefdc8f11503e83c469a
Emmanuel Awuah <emmanuelawuah.dev@gmail.com>
2025-06-17 18:36:29 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# chaleride
+
+carpooling service
---
+++ android/app/src/main/kotlin/com/oddlycodes/chaleride/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.oddlycodes.chaleride
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
